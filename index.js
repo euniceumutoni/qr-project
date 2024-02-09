c7e0a7890dd6c04a77eb3a71295cc6a83b553d4e
Eunice Umutoni <136147297+euniceumutoni@users.noreply.github.com>
2024-02-08 23:04:04 -0600

    index.js

--- index.js
+++ index.js
@@ -5,14 +5,27 @@
 */
 
 import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
+
 
 inquirer
   .prompt([
-    /* Pass your questions in here */
+    {
+      message: "Type in your URL: ",
+      name: "URL",
+    },
   ])
   .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+
+    fs.writeFile('URL.txt', url, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      }); 
+})
   .catch((error) => {
     if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
@@ -22,9 +35,7 @@ inquirer
   });
 
 
- var qr = require('qr-image');
- import xxx from"
-var qr_svg = qr.image('I love QR!', { type: 'svg' });
-qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
+
+
+
  
-var svg_string = qr.imageSync('I love QR!', { type: 'svg' });
